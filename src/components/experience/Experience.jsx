40cf561b23900ed78f5bdf0122589502cba46abe
Bhupesh <bpattaniak@gmail.com>
2023-04-10 00:07:07 +0530

    Experience section completed,

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './experience.css'
+import Skill from './Details'
+import { frontendSkills, backendSkills } from './skills'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="container experience__container">
+        <div>
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {
+              frontendSkills.map((skill,index) => {
+                return <Skill key={index} name={skill.name} level={skill.level} />
+              })
+            }
+          </div>
+        </div>
+        <div>
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            {
+              backendSkills.map((skill,index) => {
+                return <Skill key={index} name={skill.name} level={skill.level} />
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/experience/skills.js
@@ -0,0 +1,17 @@
+let frontendSkills = [
+    { name: "HTML", level: "Experienced" },
+    { name: "CSS", level: "intermediate" },
+    { name: "Javascript", level: "intermediate" },
+    { name: "React Js", level: "intermediate" },
+    { name: "Flutter", level: "Experienced" },
+]
+
+let backendSkills = [
+    { name: "SQL", level: "Experienced" },
+    { name: "Node Js", level: "intermediate" },
+    { name: "Javascript", level: "intermediate" },
+    { name: "React Js", level: "intermediate" },
+    { name: "Flutter", level: "Experienced" },
+]
+
+export { frontendSkills, backendSkills };
