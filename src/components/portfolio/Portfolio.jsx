573ded2954cbc39c3e7e02efc8937fd843b86dfd
Bhupesh <bpattaniak@gmail.com>
2023-04-10 20:48:03 +0530

    Portfolio Section Completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './portfolio.css'
+import data from './data'
+import PortfolioItem from './PortfolioItem'
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({image,title,github,demo},index)=>{
+            return <PortfolioItem key={index} image={image} title={title} github={github} demo={demo}/>
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/portfolio/data.js
@@ -0,0 +1,47 @@
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+let data = [
+    {
+        image:IMG1,
+        title:"Konnet",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+    {
+        image:IMG2,
+        title:"oabahsd",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+    {
+        image:IMG3,
+        title:"oabahsd",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+    {
+        image:IMG4,
+        title:"oabahsd",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+    {
+        image:IMG5,
+        title:"oabahsd",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+    {
+        image:IMG6,
+        title:"oabahsd",
+        github:"https://github.com",
+        demo:"https://youtube.com"
+    },
+]
+
+export default data;
